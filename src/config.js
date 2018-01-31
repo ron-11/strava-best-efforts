ae981af4233bb33e1d79795d632a7d6d019352ab
unknown <jourder@FR-L2469682.groupinfra.com>
2018-01-31 23:56:59 +0100

    init - commit initial

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// Initialize Firebase
+export default {
+	apiKey: "AIzaSyADl7GDE8OUFENyK_6LwH384j5PjGuyoOo",
+	authDomain: "strava-best-efforts-bef69.firebaseapp.com",
+	databaseURL: "https://strava-best-efforts-bef69.firebaseio.com"
+}
